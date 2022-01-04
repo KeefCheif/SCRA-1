98e1f328a3752aba34cb84b7edb8c431deadd514
Keef Cheif <Keefcheif.dev@gmail.com>
2022-01-03 22:57:15 -0700

    Initial Commit

---
+++ SCRA-1/SCRA_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SCRA_1App.swift
+//  SCRA-1
+//
+//  Created by peter allgeier on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SCRA_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
